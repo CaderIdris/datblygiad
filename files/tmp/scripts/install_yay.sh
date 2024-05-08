f3b68c46b25b1d56d5c2bc0d950e4759664249bd
61825162+CaderIdris@users.noreply.github.com <61825162+CaderIdris@users.noreply.github.com>
2024-05-08 08:55:08 +0000

    BUG: If a package is updated between installation and installing yay, build fails

--- files/tmp/scripts/install_yay.sh
+++ files/tmp/scripts/install_yay.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-
+echo "Switching directory"
 mkdir /tmp/yay
 cd /tmp/yay
+echo "Cloning Repo"
 git clone https://aur.archlinux.org/yay-bin.git
 cd yay-bin
+echo "Building yay"
 makepkg -si --noconfirm
 yay --save --answerclean All --answerdiff All
 yay --save --cleanmenu=false --diffmenu=false
