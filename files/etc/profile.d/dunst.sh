4f4e154abe26580f8e637143ccfcf0c4c0a27c58
61825162+CaderIdris@users.noreply.github.com <61825162+CaderIdris@users.noreply.github.com>
2024-05-09 19:16:01 +0000

    Need a notification manager now that container doesn't share with host

---
+++ files/etc/profile.d/dunst.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Check whether dunst is running
+dunstctl is-paused > /dev/null 2>&1
+
+# If not running, start it
+dunstctl_status=$?
+[ $dunstctl_status -ne 0 ] && dunst > /dev/null 2>&1 &
