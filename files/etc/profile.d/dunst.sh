a1f7ab317d797295e1ad5a21118e355eb1a101a6
61825162+CaderIdris@users.noreply.github.com <61825162+CaderIdris@users.noreply.github.com>
2024-06-03 16:44:39 +0000

    Updated dunst config and added new packages

--- files/etc/profile.d/dunst.sh
+++ files/etc/profile.d/dunst.sh
@@ -5,4 +5,4 @@ dunstctl is-paused > /dev/null 2>&1
 
 # If not running, start it
 dunstctl_status=$?
-[ $dunstctl_status -ne 0 ] && dunst > /dev/null 2>&1 &
+[ $dunstctl_status -ne 0 ] && (dunst > /dev/null 2>&1 &)
